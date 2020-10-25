c3930af0e9703f9326d8e0a7f30b6a620fa00a70
juliaballo <58012860+juliaballo@users.noreply.github.com>
2020-10-25 15:16:54 -0700

    Update stringMethods.js

--- stringMethods.js
+++ stringMethods.js
@@ -3,3 +3,9 @@ const message = "    TASTE THE RAINBOW!  "; //DON'T CHANGE THIS LINE PLEASE!
 
 // YOUR CODE BELOW THIS LINE:
 const whisper = message.toLowerCase().trim();
+
+// DON'T TOUCH THIS LINE! (please)
+const word = 'skateboard' //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+const facialHair = word.slice(5, 10).replace("o", "e");
